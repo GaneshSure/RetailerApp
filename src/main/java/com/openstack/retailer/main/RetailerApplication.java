a4a0b716ea0cac6c1608d54a6a3d8844aa0e8757
Rakesh <rake.kv93@gmail.com>
2017-07-04 10:00:25 +0530

    Added swagger dependency and also code changes for RESTful API Documentation Specification.

--- src/main/java/com/openstack/retailer/main/RetailerApplication.java
+++ src/main/java/com/openstack/retailer/main/RetailerApplication.java
@@ -5,8 +5,18 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Rakesh Muppa <rake.kv93@gmail.com>
@@ -18,6 +28,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = "com.openstack.*")
 @EnableJpaRepositories(basePackages = "com.openstack.retailer.repositories")
 @EntityScan(basePackages = "com.openstack.retailer.entities")
+@EnableSwagger2
 public class RetailerApplication {
 
 	static final Logger logger = LogManager.getLogger(RetailerApplication.class.getName());
@@ -26,4 +37,26 @@ public class RetailerApplication {
 		logger.info("entered application");
 		SpringApplication.run(RetailerApplication.class, args);
 	}
+	
+	@Bean
+	public Docket api() {                
+	    return new Docket(DocumentationType.SWAGGER_2)          
+	      .select()
+	      .apis(RequestHandlerSelectors.basePackage("com.openstack.retailer.controller"))
+	      
+	      .paths(PathSelectors.any())
+	      .build()
+	      .apiInfo(apiInfo());
+	}
+	 
+	private ApiInfo apiInfo() {
+	    ApiInfo apiInfo = new ApiInfoBuilder()
+	    		.title("Shopping Cart API's for Retailers Apllication")
+	    		.description("REST services are implemented using Spring Boot 'Micro Services' for Shopping Cart software.")
+	    		.contact(new Contact("Rakesh Muppa", "http://j2eearchitec.blogspot.com/", "rake.kv93@gmail.com"))
+	    		.version("1.0")
+	    		.build();
+	    return apiInfo;
+	}
+	
 }
